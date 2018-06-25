d26c3001de66c98834586e3e0c731f3b8cdb0401
icreator <icreator@mail.ru>
2018-06-25 13:49:00 +0300

    apipay utf-8

--- controllers/apipay.py
+++ controllers/apipay.py
@@ -12,17 +12,13 @@ import serv_to_buy
 
 @cache.action(time_expire=time_exp, cache_model=cache.disk) #, vars=False, public=True, lang=True)
 def index():
-    response.title = T('\u041a\u0430\u043a \u043d\u0430\u0447\u0430\u0442\u044c \u043f\u0440\u0438\u043d\u0438\u043c\u0430\u0442\u044c \u043e\u043f\u043b\u0430\u0442\u044b \u0431\u0438\u0442\u043a\u043e\u0438\u043d\u0430\u043c\u0438 \u043d\u0430 \u0441\u0430\u0439\u0442\u0435')
-    response.meta['keywords']=T('\u043e\u043f\u043b\u0442\u044b \u0431\u0438\u0442\u043a\u043e\u0438\u043d\u0430\u043c\u0438, \u043f\u043b\u0430\u0442\u0435\u0436\u0438 \u0431\u0438\u0442\u043a\u043e\u0438\u043d\u0430\u043c\u0438, \u043f\u0440\u0438\u0451\u043c \u0431\u0438\u0442\u043a\u043e\u0438\u043d\u043e\u0432, \u0441\u043a\u0440\u0438\u043f\u0442 \u0431\u0438\u0442\u043a\u043e\u0438\u043d \u0441\u0430\u0439\u0442\u0430')
-    response.meta['description']=T('\u041e\u0440\u0433\u0430\u043d\u0438\u0437\u0443\u0439 \u043f\u0440\u0438\u0451\u043c \u0431\u0438\u0442\u043a\u043e\u0438\u043d\u043e\u0432 \u043d\u0430 \u0441\u0432\u043e\u0451\u043c \u0441\u0430\u0439\u0442\u0435, \u0441\u043a\u0440\u0438\u043f\u0442 \u0431\u0438\u0442\u043a\u043e\u0438\u043d \u0441\u0430\u0439\u0442\u0430')
-    #  http://127.0.0.1:90/ipay/clients/api/get_rate?curr_in=BTC&curr_out=RUB&vol_in=0.23
-    # https://7pay.in/ipay/clients/api/get_rate?curr_in=RUB&curr_out=BTC&vol_in=230
     return dict()
 
 def mess(error):
     return '{"error": "%s"}' % error
 
 # get URI for income exchanges
+# https://127.0.0.1/apipay/get_uri/[deal_id]/[curr_in_id]/[curr_out_id]/[address_out]/[amount_out]
 def get_uri():
     import rates_lib, common
     # /deal_id/curr_in_id/curr_out_id/addr/vol
@@ -52,7 +48,6 @@ def get_uri():
 
     try:
         if len(addr_out) < 25 or len(addr_out) > 40:
-            ##\u043f\u0440\u043e\u0432\u0435\u0440\u043a\u0430 \u043d\u0430 \u0441\u0447\u0435\u0442 - \u0435\u0441\u043b\u0438 \u044d\u0442\u043e \u043d\u0435 \u0435\u043c\u0430\u0439\u043b \u0438 \u043d\u0435 \u0442\u0442\u0435\u043b\u0435\u0444\u043e\u043d \u0442\u043e \u043d\u0430\u0434\u043e \u0434\u043b\u0438\u043d\u043d\u0443 \u0438 \u043d\u0430 \u0446\u0438\u0438\u0444\u0440\u044b
             return mess('error address')
     except:
         return mess('error address')
@@ -89,7 +84,7 @@ def get_uri():
         
     #print request.application[-5:]
     if request.application[:-3] != '_dvlp':
-        # \u0447\u0435\u0442\u043e \u043a\u043e\u043d\u0444\u043b\u0438\u043a\u0442 \u0435\u0441\u043b\u0438 \u0438\u0437 ipay3_dvlp \u0432\u044b\u0437\u044b\u0432\u0430\u0442\u044c \u0442\u043e \u043a\u043e\u0448\u0435\u043b\u0435\u043a \u043d\u0430 ipay3 \u043d\u0435 \u043a\u043e\u043d\u043d\u0435\u043a\u0442\u0438\u0442\u0441\u044f
+        # чето конфликт если из ipay3_dvlp вызывать то кошелек на ipay3 не коннектится
         if token_system_out:
             curr_block = rpc_erachain.get_info(token_system_out.connect_url)
             if type(curr_block) != type(1):
@@ -128,10 +123,10 @@ def get_uri():
         pass
     else:
         x_acc_label = db_client.make_x_acc(deal, addr_out, curr_out_abbrev)
-        # \u043d\u0430\u0439\u0434\u0435\u043c \u0440\u0430\u043d\u0435\u0435 \u0441\u043e\u0437\u0434\u0430\u043d\u043d\u044b\u0439 \u0430\u0434\u0440\u0435\u0441\u0441 \u0434\u043b\u044f \u044d\u0442\u043e\u0433\u043e \u0442\u0435\u043b\u0435\u0444\u043e\u043d\u0430, \u044d\u0442\u043e\u0439 \u043a\u0440\u0438\u043f\u0442\u044b \u0438 \u044d\u0442\u043e\u0433\u043e \u0444\u0438\u0430\u0442\u0430
-        # \u0441\u043d\u0430\u0447\u0430\u043b\u0430 \u043d\u0430\u0439\u0442\u0435\u043c \u0430\u043a\u043a\u0430\u0443\u043d\u0442 \u0443 \u0434\u0435\u043b\u0430
+        # найдем ранее созданный адресс для этого телефона, этой крипты и этого фиата
+        # сначала найтем аккаунт у дела
         deal_acc_id = db_client.get_deal_acc_id(db, deal, addr_out, curr_out)
-        # \u0442\u0435\u043f\u0435\u0440\u044c \u043d\u0430\u0439\u0434\u0435\u043c \u043a\u043e\u0448\u0435\u043b\u0435\u043a \u0434\u043b\u044f \u0434\u0430\u043d\u043d\u043e\u0439 \u043a\u0440\u0438\u043f\u0442\u044b
+        # теперь найдем кошелек для данной крипты
         deal_acc_addr = db_client.get_deal_acc_addr_for_xcurr(db, deal_acc_id, curr_in, xcurr_in, x_acc_label)
         if not deal_acc_addr:
             return mess('Connection to [%s] id lost, try lates ' % curr_in_name)
@@ -139,12 +134,11 @@ def get_uri():
         addr_in = deal_acc_addr.addr
         
 
-    # \u0435\u0441\u043b\u0438 \u0435\u0441\u0442\u044c \u0441\u043a\u0440\u044b\u0442\u044b\u0439 \u043f\u0430\u0440\u0442\u043d\u0435\u0440\u0441\u043a\u0438\u0439 \u043a\u043e\u0434 \u0442\u043e \u0435\u0433\u043e \u0437\u0430\u0431\u044c\u0435\u043c \u043f\u043e\u043b\u044c\u0437\u043e\u0432\u0430\u0442\u0435\u043b\u044e
     deal_acc = db.deal_accs[deal_acc_id]
     volume_out = vol
 
-    # \u0438\u0441\u043f\u043e\u043b\u044c\u0437\u0443\u0435\u043c \u0431\u044b\u0441\u0442\u0440\u044b\u0439 \u043f\u043e\u0438\u0441\u043a \u043a\u0443\u0440\u0441\u0430 \u043f\u043e \u0444\u043e\u0440\u043c\u0443\u043b\u0435 \u0441\u043e \u0441\u0442\u0435\u043f\u0435\u043d\u044c\u044e \u043d\u0430 \u043a\u043e\u043b\u0438\u0447\u0435\u0441\u0442\u0432\u043e \u0432\u0445\u043e\u0434\u0430
-    # \u0442\u043e\u043b\u044c\u043a\u043e \u043d\u0430\u0434\u043e \u043d\u0430\u0439\u0442\u0438 \u043a\u043e\u043b-\u0432\u043e \u0432\u0445\u043e\u0434\u0430 \u043e\u0442 \u0432\u044b\u0445\u043e\u0434\u0430
+    # используем быстрый поиск курса по формуле со степенью на количество входа
+    # только надо найти кол-во входа от выхода
     pr_b, pr_s, pr_avg = rates_lib.get_average_rate_bsa(db, curr_in.id, curr_out.id, None)
     if pr_avg:
         vol_in = volume_out / pr_b
@@ -155,12 +149,12 @@ def get_uri():
     if best_rate:
         is_order = True
         dealer_deal = None
-        # \u0441\u043d\u0430\u0447\u0430\u043b\u0430 \u043e\u0442\u043a\u0440\u0443\u0442\u0438\u043c \u043e\u0431\u0440\u0430\u0442\u043d\u0443\u044e \u0442\u0430\u043a\u0441\u0443
+        # сначала открутим обратную таксу
         txfee = float(xcurr_out.txfee or 0.0001)
         volume_out += txfee
         volume_in, mess_in = db_client.calc_fees_back(db, deal, dealer_deal, curr_in, curr_out, volume_out,
                                            best_rate, is_order, note=0)
-        ## \u0442\u0435\u043f\u0435\u0440\u044c \u0442\u0430\u043a\u0441\u044b \u0434\u043b\u044f \u0447\u0435\u043b\u043e\u0432\u0435\u043a\u0430 \u043f\u043e\u043b\u0443\u0447\u0438\u043c \u0438 \u0434\u043e\u043b\u0436\u043d\u0430 \u0442\u0430 \u0436\u0435 \u0446\u0438\u0444\u0440\u0430 \u0432\u044b\u0439\u0442\u0438
+        ## теперь таксы для человека получим и должна та же цифра выйти
         vol_out_new, tax_rep = db_client.calc_fees(db, deal, dealer_deal, curr_in, curr_out, volume_in,
                                            best_rate, is_order, note=1)
         vol_out_new = common.rnd_8(vol_out_new)
@@ -176,7 +170,7 @@ def get_uri():
             volume_in = volume_in,
             volume_out = volume_out,
             )
-        # \u0442\u0435\u043f\u0435\u0440\u044c \u0441\u0442\u0435\u043a \u0434\u043e\u0431\u0430\u0432\u0438\u043c, \u043a\u043e\u0442\u043e\u0440\u044b\u0439 \u0431\u0443\u0434\u0435\u043c \u0443\u0434\u0430\u043b\u044f\u0442\u044c \u043f\u043e\u0442\u043e\u043c
+        # теперь стек добавим, который будем удалять потом
         db.orders_stack.insert( ref_ = order_id )
     else:
         volume_in = rate_out = tax_rep = None
@@ -184,7 +178,8 @@ def get_uri():
     _, url_uri = common.uri_make( curr_in.name2, addr_in, {'amount':volume_in, 'label': db_client.make_x_acc_label(deal, addr_out, curr_out_abbrev)})
 
     curr_in_abbrev = curr_in.abbrev
-    # \u043e\u0433\u0440\u0430\u043d\u0438\u0447\u0435\u043d\u0438\u0435 \u043d\u0430 \u0432\u0445\u043e\u0434 \u0438 \u043e\u0441\u0442\u0430\u0442\u043e\u043a \u043d\u0430 \u043f\u0440\u0435\u0432\u044b\u0448\u0435\u043d\u0438\u0435
+    
+    #
     lim_bal, may_pay = db_client.is_limited_ball(curr_in)
 
     free_bal = db_client.curr_free_bal(curr_out)
