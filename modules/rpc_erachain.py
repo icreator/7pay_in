04bb7446deccc99de657f436f0ec5594cd3ec4b3
icreator <icreator@mail.ru>
2020-11-15 11:15:45 +0300

    xcurr.main_addr

--- modules/rpc_erachain.py
+++ modules/rpc_erachain.py
@@ -3,6 +3,9 @@
 
 import urllib
 import urllib2
+
+from gluon import current
+
 #import json
 from gluon.contrib import simplejson as json
 import time
@@ -30,7 +33,7 @@ def rpc_request(pars, vars=None, test=None):
 
     ##rq.add_header('Authorization', 'Bearer ' + token)
     #return rq
-        # платеж в процессе - ожидаем и потом еще раз запросим
+    # платеж в процессе - ожидаем и потом еще раз запросим
     try:
         f = urllib2.urlopen(rq)
         #r = f.read()
@@ -38,7 +41,6 @@ def rpc_request(pars, vars=None, test=None):
 
         #print 'response - res:', r
     except Exception as e:
-        from gluon import current
         # или любая ошибка - повтор запроса - там должно выдать ОК
         #print 'YmToConfirm while EXEPTION:', e
         log(current.db, 'rpc ' + pars + ' EXCEPTION: %s' % e)
@@ -58,12 +60,12 @@ def get_deal_acc_addr(db, deal_id, curr_out, acc, addr, xcurr_in):
         deal_acc_id = deal_acc.id
 
     deal_acc_addr = db((db.deal_acc_addrs.addr==addr)
-        & (db.deal_acc_addrs.xcurr_id==xcurr_in.id)).select().first()
+                       & (db.deal_acc_addrs.xcurr_id==xcurr_in.id)).select().first()
 
     if not deal_acc_addr:
         deal_acc_addrs_id = db.deal_acc_addrs.insert(deal_acc_id = deal_acc_id, addr=addr, xcurr_id=xcurr_in.id)
         deal_acc_addr = db.deal_acc_addrs[ deal_acc_addrs_id ]
-        
+
     return deal_acc_id, deal_acc_addr
 
 
@@ -77,12 +79,12 @@ def is_not_valid_addr(rpc_url, addr):
     res = rpc_request(rpc_url + "/addresses/validate/" + addr)
     return not res
 
-    
+
 def get_balances(rpc_url, addr):
-    
+
     res = rpc_request(rpc_url + "/addresses/assets/" + addr)
     #res = rpc_request(rpc_url + "/addresses/balance/" + addr)
-    
+
     return res
 
 def get_reserve(token_system, token):
@@ -100,7 +102,7 @@ def get_tx_info(token_system, txid):
     return recs
 
 def get_transactions(token_system, rpc_url, addr, from_block=2, conf=2):
-    
+
     result = []
 
     height = rpc_request(rpc_url + "/blocks/height")
@@ -110,7 +112,7 @@ def get_transactions(token_system, rpc_url, addr, from_block=2, conf=2):
         return result, None
 
     i = from_block
-    
+
     ## TODO + confirmed HARD
     while i + conf <= height:
         if len(result) > 100 or i - from_block > 30000:
@@ -126,7 +128,6 @@ def get_transactions(token_system, rpc_url, addr, from_block=2, conf=2):
         except Exception as e:
             print e
             print recs
-            from gluon import current
             log(current.db, 'get_transactions %s EXCEPTION: %s - %s' % (url_get, e, recs))
             return result, i - 1
 
@@ -135,7 +136,7 @@ def get_transactions(token_system, rpc_url, addr, from_block=2, conf=2):
             print 'erachain incomes - height: ', i, ' recs:', len(recs)
         else:
             continue
-            
+
         incomes = []
         for rec in recs:
             #print rec
@@ -151,38 +152,38 @@ def get_transactions(token_system, rpc_url, addr, from_block=2, conf=2):
             if rec.get('title') == '.main.':
                 ## skip my deposit
                 continue
-            
+
             incomes.append(rec)
             #print 'erachain - title:', rec.get('title'), 'message:', rec.get('message')
-            
+
         result += incomes
-    
+
     return result, i
 
 
 def send(db, curr, xcurr, addr, amo, token_system=None, token=None, title=None, mess=None):
-    
+
     if token is None:
         token = db.tokens[xcurr.as_token]
     if token_system is None:
         token_system = db.systems[token.system_id]
-        
+
     amo = round(float(amo),8)
-    
+
     if token.token_key == 2:
         # if it is COMPU
         txfee = round(float(token_system.txfee or 0.0001), 8)
     else:
         txfee = 0
-    
+
     try:
         reserve = get_reserve(token_system, token)
     except Exception as e:
         return {'error': 'connection lost - [%s]' % curr.abbrev }, None
-        
+
     if amo + txfee > reserve:
         return {'error':'out off reserve:[%s]' % reserve }, None
-    
+
     # проверим готовность базы - is lock - и запишем за одно данные
     log_commit(db, 'try send: %s[%s] %s, fee: %s' % (amo, curr.abbrev, addr, txfee))
     if amo > txfee * 2:
@@ -194,9 +195,9 @@ def send(db, curr, xcurr, addr, amo, token_system=None, token=None, title=None, 
             if False: ## 4.11 version Erachain
                 # GET r_send/7GvWSpPr4Jbv683KFB5WtrCCJJa6M36QEP/79MXsjo9DEaxzu6kSvJUauLhmQrB4WogsH?message=mess&encrypt=false&password=123456789
                 pars = "r_send/%s/%s?assetKey=%d&amount=%f&title=%s%s&encrypt=true&password=%s" % (token_system.account, addr,
-                                   token.token_key, amo_to_pay,
-                                   title or 'face2face.cash', mess and ('&message='+mess) or '',
-                                   token_system.password)
+                                                                                                   token.token_key, amo_to_pay,
+                                                                                                   title or 'face2face.cash', mess and ('&message='+mess) or '',
+                                                                                                   token_system.password)
                 print pars
                 res = rpc_request(token_system.connect_url + pars)
             else:
@@ -212,7 +213,7 @@ def send(db, curr, xcurr, addr, amo, token_system=None, token=None, title=None, 
             if error:
                 error_message = current.CODE_UTF and str(res['message'] + ('%s' % error)).decode(current.CODE_UTF,'replace') or str(res['message'] + ('%s' % error))
                 return {'error': error_message  + ' [%s]' % curr.abbrev }, None
-            
+
             res = res['signature']
 
         #else:
